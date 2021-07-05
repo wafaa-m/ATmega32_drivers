2578fe683a09202d405c21a84b3471ccedfa8d82
wafaa-m <76871512+wafaa-m@users.noreply.github.com>
2021-07-06 00:29:44 +0200

    Push buttons modules
    Push buttons modules configuration, and programing files

---
+++ PUSH_BUTTON_CONFIG.h
@@ -0,0 +1,23 @@
+/*
+ * PUSH_BUTTON_CONFIG.h
+ *
+ * Created: 5/30/2021 5:04:15 AM
+ *  Author: Wafaa
+ */ 
+
+
+#ifndef PUSH_BUTTON_CONFIG_H_
+#define PUSH_BUTTON_CONFIG_H_
+
+#include "CPU_CONFIGRATIONS.h"
+
+#define BUTTON0_PORT       DIO_PORTB
+#define BUTTON0_PIN        DIO_PIN0
+
+#define BUTTON1_PORT       DIO_PORTD
+#define BUTTON1_PIN        DIO_PIN6
+
+#define BUTTON2_PORT       DIO_PORTD
+#define BUTTON2_PIN        DIO_PIN2
+
+#endif /* PUSH_BUTTON_CONFIG_H_ */
